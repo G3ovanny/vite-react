04eb51f0b1226349347688e4af0f1d441edd9041
Geovanny Lara <jeffersonlara98@gmail.com>
2024-04-03 21:58:42 -0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),],
-  // Opcional: configuración adicional para manejar otros tipos de archivos de imagen
-  resolve: {
-    alias: {
-      '@/assets': '/src/assets' // Alias para la carpeta de assets
-    }
-  }
-})
+  plugins: [react(), splitVendorChunkPlugin()],
+});
