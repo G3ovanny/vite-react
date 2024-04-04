156ad28f8c59d4fa7d6f186690227ad2446f96a1
Geovanny Lara <jeffersonlara98@gmail.com>
2024-04-03 22:24:46 -0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import image from './assets/code.webp'
 
 function App() {
   return (
     <>
       <h1>Hello World</h1>
-      <img src="src\assets\code.webp" alt="" />
+      <img src={image} alt="" />
     </>
   )
 }
